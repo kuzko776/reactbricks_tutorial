239c7445caa45b73221019076806d0904264bcc9
kuzko <kuzko779@gmail.com>
2022-07-19 16:12:20 +0200

    First

---
+++ react-bricks/bricks/Thumbnail.tsx
@@ -0,0 +1,21 @@
+// Thumbnail.tsx
+import React from 'react'
+import { types, Text } from 'react-bricks/frontend'
+
+const Thumbnail: types.Brick = () => {
+  return <div className="p-6 text-center">
+       <Text
+        propName="title"
+        renderBlock={({ children }) => <h1 className="text-2xl font-bold">{children}</h1>}
+        placeholder="Type a title..."
+      />
+  </div>
+}
+
+Thumbnail.schema = {
+  name: 'thumbnail',
+  label: 'Thumbnail',
+  sideEditProps: [],
+}
+
+export default Thumbnail
